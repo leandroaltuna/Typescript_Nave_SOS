d4662e85170ce5d55f8a898b725410f0c004f75e
Leandro <leandroaltuna.dev@gmail.com>
2025-06-30 01:02:45 -0300

    2nd change-diagram

--- src/services/changeDiagram.service.ts
+++ src/services/changeDiagram.service.ts
@@ -13,8 +13,10 @@ export function getChangeDiagram( pressure: number ) {
         const VV = calcVV( pressure );
     
         const Result = {
-            'specific_volume_liquid': ( roundTemp <= 30 ) ? null : parseFloat( VL.toFixed( 5 ) ),
-            'specific_volume_vapor': ( roundTemp <= 30 ) ? null : parseFloat( VV.toFixed( 5 ) )
+            // 'specific_volume_liquid': ( roundTemp <= 30 ) ? null : parseFloat( VL.toFixed( 5 ) ),
+            // 'specific_volume_vapor': ( roundTemp <= 30 ) ? null : parseFloat( VV.toFixed( 5 ) )
+            'specific_volume_liquid': parseFloat( VL.toFixed( 5 ) ),
+            'specific_volume_vapor': parseFloat( VV.toFixed( 5 ) )
         };
     
         return Result;
